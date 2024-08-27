def3f2d911d3d032e1107140fbc4c04235eee68f
YuJeeun <dbwpdms1122@naver.com>
2024-08-27 17:04:39 +0900

    ✨ 기능 추가: Employee, Employer DTO 추가

--- src/main/java/com/intbyte/wizbuddy/user/dto/EmployeeDTO.java
+++ src/main/java/com/intbyte/wizbuddy/user/dto/EmployeeDTO.java
@@ -1,4 +1,31 @@
 package com.intbyte.wizbuddy.user.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class EmployeeDTO {
+    private int employeeCode;
+
+    private String employeeName;
+
+    private String employeeEmail;
+
+    private String employeePassword;
+
+    private String employeePhone;
+
+    private boolean employeeFlag;
+
+    private boolean employeeBlackState;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/intbyte/wizbuddy/user/dto/EmployerDTO.java
+++ src/main/java/com/intbyte/wizbuddy/user/dto/EmployerDTO.java
@@ -1,4 +1,33 @@
 package com.intbyte.wizbuddy.user.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class EmployerDTO {
+
+    private int employerCode;
+
+    private String employerName;
+
+    private String employerEmail;
+
+    private String employerPassword;
+
+    private String employerPhone;
+
+    private boolean employerFlag;
+
+    private boolean employerBlackState;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
