c5bbb219a2b79ee24a6f2ccba1e51fb2865a94c2
YuJeeun <dbwpdms1122@naver.com>
2024-08-27 16:00:55 +0900

    ✨ 기능 추가: ManualBoard 엔티티 추가

--- src/main/java/com/intbyte/wizbuddy/board/domain/entity/ManualBoard.java
+++ src/main/java/com/intbyte/wizbuddy/board/domain/entity/ManualBoard.java
@@ -1,4 +1,43 @@
 package com.intbyte.wizbuddy.board.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.time.LocalDateTime;
+
+@Entity(name = "manualboard")
+@Table(name = "manualboard")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ManualBoard {
+    @Id
+    @Column
+    private int ManualCode;
+
+    @Column
+    private String ManualTitle;
+
+    @Column
+    private String ManualContents;
+
+    @Column
+    private boolean ManualFlag;
+
+    @Column
+    private String ImageURL;
+
+    @Column
+    private LocalDateTime CreatedAt;
+
+    @Column
+    private LocalDateTime UpdatedAt;
+
+    // 외래키 어케하죠?
+    @Column
+    private int ShopCode;
+
 }
