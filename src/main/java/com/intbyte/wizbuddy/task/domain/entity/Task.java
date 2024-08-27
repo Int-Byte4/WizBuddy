73aa649428d8ca3032739d2f34a5c39f28cacfac
seohyun <694090@gmail.com>
2024-08-27 15:35:03 +0900

    ✨ 기능 추가 : Task 엔티티 추가

--- src/main/java/com/intbyte/wizbuddy/task/domain/entity/Task.java
+++ src/main/java/com/intbyte/wizbuddy/task/domain/entity/Task.java
@@ -1,4 +1,40 @@
 package com.intbyte.wizbuddy.task.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
 public class Task {
+
+    @Id
+    @Column
+    private int TaskCode;
+
+    @Column
+    private String TaskContents;
+
+    @Column
+    private boolean TaskFlag;
+
+    @Column
+    private boolean TaskFixedState;
+
+    @Column
+    private LocalDateTime CreatedAt;
+
+    @Column
+    private LocalDateTime UpdatedAt;
 }
