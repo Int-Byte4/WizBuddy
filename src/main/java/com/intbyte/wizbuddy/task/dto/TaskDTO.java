558e7744b9d5ead30d3ad3597f4b3ccb293d2793
seohyun <694090@gmail.com>
2024-08-27 16:54:25 +0900

    ✨ 기능 추가 : TaskDTO 추가

--- src/main/java/com/intbyte/wizbuddy/task/dto/TaskDTO.java
+++ src/main/java/com/intbyte/wizbuddy/task/dto/TaskDTO.java
@@ -1,4 +1,25 @@
 package com.intbyte.wizbuddy.task.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class TaskDTO {
+
+    private int TaskCode;
+
+    private String TaskContents;
+
+    private boolean TaskFlag;
+
+    private boolean TaskFixedState;
+
+    private LocalDateTime CreatedAt;
+
+    private LocalDateTime UpdatedAt;
 }
