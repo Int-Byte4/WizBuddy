f7f7b9af8ece7761bfcb6df0e7bc4ffcca0a6c34
seohyun <694090@gmail.com>
2024-08-28 20:02:33 +0900

    ✨ 기능 추가 : NoticeBoardService 코드 추가, NoticeBoardMapper 추가

--- src/main/java/com/intbyte/wizbuddy/board/service/NoticeBoardService.java
+++ src/main/java/com/intbyte/wizbuddy/board/service/NoticeBoardService.java
@@ -1,4 +1,10 @@
 package com.intbyte.wizbuddy.board.service;
 
+import com.intbyte.wizbuddy.mapper.NoticeBoardMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoticeBoardService {
+
+    private NoticeBoardMapper noticeBoardMapper;
 }
