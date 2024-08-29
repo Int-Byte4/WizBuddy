1d911bed464dfc8771ec09c9724a3dc455518887
Hellin22 <dno06117@naver.com>
2024-08-29 20:15:25 +0900

    ✨ 기능 수정: TaskService 메서드 기능 변경
    crud 메서드 수정
     DTO, Entity에 더해 EditTaskInfo 추가 사용

--- src/main/java/com/intbyte/wizbuddy/task/service/TaskService.java
+++ src/main/java/com/intbyte/wizbuddy/task/service/TaskService.java
@@ -1,6 +1,8 @@
 package com.intbyte.wizbuddy.task.service;
 
+import com.intbyte.wizbuddy.exception.task.TaskNotFoundException;
 import com.intbyte.wizbuddy.mapper.TaskMapper;
+import com.intbyte.wizbuddy.task.domain.EditTaskInfo;
 import com.intbyte.wizbuddy.task.domain.entity.Task;
 import com.intbyte.wizbuddy.task.dto.TaskDTO;
 import com.intbyte.wizbuddy.task.repository.TaskRepository;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,90 +19,64 @@ import java.util.stream.Collectors;
 public class TaskService {
 
     private final TaskRepository taskRepository; // jpa
-    private final TaskMapper mapper;        // mybatis
+    private final TaskMapper taskMapper;        // mybatis
     private final ModelMapper modelMapper; // dto <-> entity 변환
 
     @Autowired
     public TaskService(TaskRepository taskRepository, TaskMapper mapper, ModelMapper modelMapper) {
         this.taskRepository = taskRepository;
-        this.mapper = mapper;
+        this.taskMapper = mapper;
         this.modelMapper = modelMapper;
     }
 
-    // task는 mapper 사용하기
-    // checklist는 mapper 사용 x로 진행.
-
-    // 1. 업무 등록을 해야함. -> 사용자에게서 TaskDTO가 넘어올거임. -> 리턴도 DTO로 해야함. -> 등록은 void?
-    // void로 하지말고 dto에 등록된 업무를 보내주는게 맞을듯? dto 리턴이 맞는거같다.
     @Transactional
-    public TaskDTO insertTask(TaskDTO taskDTO) {
-        Task insertTask = new Task(taskDTO.getTaskCode(), taskDTO.getTaskContents(), taskDTO.isTaskFlag()
-                            , taskDTO.isTaskFixedState(), taskDTO.getCreatedAt(), taskDTO.getUpdatedAt());
+    public void insertTask(TaskDTO taskInfo) {
 
-        //        modelMapper.map(taskDTO, insertTask);
-        //        Task insertTask1 = modelMapper.map(taskDTO, Task.class); //ModelMapper는 필드명이 같다면 자동으로 매핑해주며, 복잡한 객체 매핑도 가능합니다.
+        Task task = Task.builder()
+                .taskContents(taskInfo.getTaskContents())
+                .taskFlag(true)
+                .taskFixedState(taskInfo.isTaskFixedState())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(taskInfo.getUpdatedAt())
+                .build();
 
-        taskRepository.save(insertTask);
-
-        return modelMapper.map(insertTask, TaskDTO.class);
+        taskRepository.save(task);
     }
 
-    // 2. 업무 조회 (id로 task 조회, 모든 업무 조회)
-    // 2-1. id로 1개의 task 조회
     @Transactional
-    public TaskDTO findTaskById(int taskCode){ // by mybatis
+    public TaskDTO findTaskById(int taskCode){
 
-        Task findTask = mapper.findTaskById(taskCode);
+        Task findTask = taskMapper.findTaskById(taskCode);
         return modelMapper.map(findTask, TaskDTO.class);
-}
+    }
 
-    // 2-2. 전체 task 조회
     @Transactional
     public List<TaskDTO> findAllTask(){
 
-        List<Task> findTasks = mapper.findAllTask();
+        List<Task> findTasks = taskMapper.findAllTask();
 
         return findTasks.stream()
                 .map(task -> modelMapper.map(task, TaskDTO.class))
                 .collect(Collectors.toList());
     }
 
-    // 3. 업무 수정(id값에 해당하는 업무 수정)
     @Transactional
-    public TaskDTO modifyTask(TaskDTO taskDTO){
+    public void modifyTask(int taskCode, EditTaskInfo modifyTaskInfo){
 
-        int taskCode = taskDTO.getTaskCode();
-        Task task = taskRepository.findById(taskCode).orElseThrow(IllegalArgumentException::new);
-        // task를 taskdto로 수정
+        Task task = taskRepository.findById(taskCode).orElseThrow(TaskNotFoundException::new);
 
-        modelMapper.map(taskDTO, task); // task를 taskdto로 수정
-
-        Task updatedTask = taskRepository.save(task);
-
-        return modelMapper.map(updatedTask, TaskDTO.class);
+        task.modify(modifyTaskInfo);
+        taskRepository.save(task);
     }
 
-    // 4. 업무 삭제 (id값에 해당하는 task 삭제)
     @Transactional
-    public TaskDTO deleteTask(TaskDTO taskDTO){
+    public void deleteTask(int taskCode){
 
-        int taskCode = taskDTO.getTaskCode();
-        Task deleteTask = taskRepository.findById(taskCode).orElseThrow(IllegalArgumentException::new);
+        Task task = taskMapper.findTaskById(taskCode);
+        EditTaskInfo editTaskInfo = new EditTaskInfo(task.getTaskContents(), false, task.isTaskFixedState(), task.getUpdatedAt());
 
-        // 1. taskDTO로 바꿔주기
-        TaskDTO deletedTaskDTO = modelMapper.map(deleteTask, TaskDTO.class);
-        deletedTaskDTO.setTaskFlag(false);
+        task.modify(editTaskInfo);
 
-        // 2. task로 바꿔주기
-        Task deleteTask2 = new Task(deletedTaskDTO.getTaskCode(), deletedTaskDTO.getTaskContents(), deletedTaskDTO.isTaskFlag(),
-                deletedTaskDTO.isTaskFixedState(), deletedTaskDTO.getCreatedAt(), deletedTaskDTO.getUpdatedAt());
-
-        taskRepository.save(deleteTask2);
-
-        return deletedTaskDTO;
+        taskRepository.save(task);
     }
-
-    // 일단 반환값과 파라미터값을 어떻게 할지 안정한거같아서 모두 통일함. (return = TaskDTO / parameter = TaskDTO)
-    // ------------
-    // 5. 체크리스트
 }
