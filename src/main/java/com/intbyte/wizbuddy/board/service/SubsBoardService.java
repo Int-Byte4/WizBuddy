355046e7920a85421ff54d608ec17af19b0350ef
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-30 16:52:10 +0900

    ✨ 기능 추가 : SubsBoard 전체조회 Service기능 추가

--- src/main/java/com/intbyte/wizbuddy/board/service/SubsBoardService.java
+++ src/main/java/com/intbyte/wizbuddy/board/service/SubsBoardService.java
@@ -1,4 +1,37 @@
 package com.intbyte.wizbuddy.board.service;
 
+import com.intbyte.wizbuddy.board.domain.entity.SubsBoard;
+import com.intbyte.wizbuddy.board.dto.SubsBoardDTO;
+import com.intbyte.wizbuddy.board.repository.SubsBoardRepository;
+import com.intbyte.wizbuddy.mapper.SubsBoardMapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class SubsBoardService {
+
+    private final SubsBoardMapper subsBoardMapper;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public SubsBoardService(SubsBoardMapper subsBoardMapper, ModelMapper modelMapper) {
+        this.subsBoardMapper = subsBoardMapper;
+        this.modelMapper = modelMapper;
+    }
+
+
+    public List<SubsBoardDTO> findAllSubsBoards() {
+        List<SubsBoard> subsBoardList = subsBoardMapper.selectAllSubsBoard();
+
+        return subsBoardList.stream()
+                .map(subsBoard -> modelMapper.map(subsBoard, SubsBoardDTO.class))
+                .collect(Collectors.toList());
+    }
+
+
 }
