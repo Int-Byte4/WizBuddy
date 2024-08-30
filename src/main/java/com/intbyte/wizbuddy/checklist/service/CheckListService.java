a14a786e9cf6877efc220a5fa985d83632fbffc8
Hellin22 <dno06117@naver.com>
2024-08-30 09:23:05 +0900

    ✨ 기능 추가: CheckListService
    기본적인 CheckList crud 메서드 추가

--- src/main/java/com/intbyte/wizbuddy/checklist/service/CheckListService.java
+++ src/main/java/com/intbyte/wizbuddy/checklist/service/CheckListService.java
@@ -1,4 +1,85 @@
 package com.intbyte.wizbuddy.checklist.service;
 
+import com.intbyte.wizbuddy.checklist.domain.EditCheckListInfo;
+import com.intbyte.wizbuddy.checklist.domain.entity.CheckList;
+import com.intbyte.wizbuddy.checklist.dto.CheckListDTO;
+import com.intbyte.wizbuddy.checklist.repository.CheckListRepository;
+import com.intbyte.wizbuddy.exception.checklist.CheckListNotFoundException;
+import com.intbyte.wizbuddy.mapper.CheckListMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class CheckListService {
+
+    private final CheckListRepository checkListRepository;
+    private final CheckListMapper checkListMapper;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public CheckListService(CheckListRepository checkListRepository, CheckListMapper checkListMapper, ModelMapper modelMapper) {
+        this.checkListRepository = checkListRepository;
+        this.checkListMapper = checkListMapper;
+        this.modelMapper = modelMapper;
+    }
+
+    // 체크리스트 등록
+    @Transactional
+    public void insertCheckList(CheckListDTO checkListInfo){
+
+        CheckList checkList = CheckList.builder()
+                .checkListTitle(checkListInfo.getCheckListTitle())
+                .checkListFlag(true)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        checkListRepository.save(checkList);
+    }
+
+    //    체크리스트 조회
+    @Transactional
+    public CheckListDTO findCheckListById(int checkListCode){
+
+        CheckList checkList = checkListMapper.findCheckListById(checkListCode);
+        return modelMapper.map(checkList, CheckListDTO.class);
+    }
+
+    @Transactional
+    public List<CheckListDTO> findAllCheckList(){
+        List<CheckList> checkListList = checkListMapper.findAllCheckList();
+
+        return checkListList.stream()
+                .map(checkList -> modelMapper.map(checkList, CheckListDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    //    체크리스트 업무 수정
+    @Transactional
+    public void modifyCheckList(int checkListCode, EditCheckListInfo modifyCheckListInfo){
+        CheckList checkList = null;
+        try{
+            checkList = checkListMapper.findCheckListById(checkListCode);
+        }catch (Exception e){
+            throw new CheckListNotFoundException();
+        }
+
+        checkList.modify(modifyCheckListInfo);
+        checkListRepository.save(checkList);
+    }
+
+//    체크리스트 업무 삭제
+    @Transactional
+    public void deleteCheckList(int checkListCode){
+        CheckList checkList = checkListMapper.findCheckListById(checkListCode);
+        EditCheckListInfo editCheckListInfo = new EditCheckListInfo();
+    }
+
+//    체크리스트 업무 완료 표시
 }
