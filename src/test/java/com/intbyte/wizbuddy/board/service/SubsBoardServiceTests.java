347d2d16826db6f0f40ac7074736e756082edc89
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-30 17:08:48 +0900

    ✨ 기능 추가 : SubsBoard 매장별 조회 테스트 기능 추가

--- src/test/java/com/intbyte/wizbuddy/board/service/SubsBoardServiceTests.java
+++ src/test/java/com/intbyte/wizbuddy/board/service/SubsBoardServiceTests.java
@@ -1,6 +1,5 @@
 package com.intbyte.wizbuddy.board.service;
 
-
 import com.intbyte.wizbuddy.board.dto.SubsBoardDTO;
 
 import com.intbyte.wizbuddy.board.repository.SubsBoardRepository;
@@ -9,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SubsBoardServiceTests {
@@ -31,5 +32,22 @@ class SubsBoardServiceTests {
                 });
 
     }
+
+    @Test
+    public void 매장별_대타게시판_조회_테스트() {
+        // given
+        int subsCode = 1;
+
+        // when
+        SubsBoardDTO foundSubsBoard = subsBoardService.findSubsBoardById(subsCode);
+
+        // then
+        assertNotNull(foundSubsBoard, "해당 subsCode에 대한 대타게시판 항목을 찾을 수 없습니다.");
+        assertEquals(subsCode, foundSubsBoard.getSubsCode());
+
+        System.out.println("조회된 게시판 제목: " + foundSubsBoard.getSubsTitle());
+        System.out.println("조회된 게시판 내용: " + foundSubsBoard.getSubsContent());
+    }
+
 
 }
