ebc5435bdc1adeebbbf08ab77ede1f2fa45eb5aa
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-08-30 16:33:53 +0900

    ✨ 기능 추가 : 직원 조회, 수정, 삭제 기능 추가

---
+++ src/main/java/com/intbyte/wizbuddy/mapper/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.intbyte.wizbuddy.mapper;
+
+import com.intbyte.wizbuddy.user.domain.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EmployeeMapper {
+    Employee getEmployee(int employeeCode);
+}
---
+++ src/main/java/com/intbyte/wizbuddy/user/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.intbyte.wizbuddy.user.repository;
+
+import com.intbyte.wizbuddy.user.domain.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
