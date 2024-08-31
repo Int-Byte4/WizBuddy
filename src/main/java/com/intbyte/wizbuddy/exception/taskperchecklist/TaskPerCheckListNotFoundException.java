54d90783052241f5fbced1515e806beef1e5f761
Hellin22 <dno06117@naver.com>
2024-09-01 00:31:52 +0900

    ✨ 기능 추가: TaskPerCheckList 예외처리 추가

--- src/main/java/com/intbyte/wizbuddy/exception/taskperchecklist/TaskPerCheckListNotFoundException.java
+++ src/main/java/com/intbyte/wizbuddy/exception/taskperchecklist/TaskPerCheckListNotFoundException.java
@@ -6,10 +6,10 @@ public class TaskPerCheckListNotFoundException extends IllegalArgumentException 
 
     private final StatusEnum status;
 
-    private static final String message = "해당 업무가 존재하지 않습니다.";
+    private static final String message = "해당 업무별 체크리스트가 존재하지 않습니다.";
 
     public TaskPerCheckListNotFoundException() {
         super(message);
-        this.status = StatusEnum.TASK_NOT_FOUND;
+        this.status = StatusEnum.TASK_PER_CHECKLIST_NOT_FOUND;
     }
 }
