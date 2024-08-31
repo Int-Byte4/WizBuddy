8631f7f8a608551293724f187d7ee40fd6ec0595
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-31 18:54:51 +0900

    ✨ 기능 수정 : Comment Entity 기능 수정

--- src/main/java/com/intbyte/wizbuddy/comment/domain/Entity/Comment.java
+++ src/main/java/com/intbyte/wizbuddy/comment/domain/Entity/Comment.java
@@ -1,21 +1,25 @@
 package com.intbyte.wizbuddy.comment.domain.Entity;
 
 
+import com.intbyte.wizbuddy.comment.domain.EditCommentInfo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
 
 @Entity (name = "comment")
 @Table (name = "comments")
-@Setter
+@Builder
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@EqualsAndHashCode
 public class Comment {
 
     @Id
@@ -23,23 +27,35 @@ public class Comment {
     private int commentCode;
 
     @Column(name = "comment_contents")
-    private String comment_contents;
+    private String commentContent;
 
     @Column(name = "comment_flag")
-    private boolean comment_flag;
+    @ColumnDefault("true")
+    private boolean commentFlag;
 
     @Column(name = "comment_adopted_state")
-    private boolean comment_adopted_state;
+    private boolean commentAdoptedState;
 
     @Column(name = "created_at")
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
-    private LocalDateTime updated_at;
+    private LocalDateTime updatedAt;
 
     @Column(name = "subs_code")
     private int subsCode;
 
     @Column(name = "employee_code")
     private int employeeCode;
+
+    public void toUpdate(@Valid EditCommentInfo editCommentInfo) {
+        this.commentContent = editCommentInfo.getCommentContent();
+        this.commentAdoptedState = editCommentInfo.isCommentAdoptedState();
+        this.createdAt = editCommentInfo.getCreatedAt();
+        this.updatedAt = editCommentInfo.getUpdatedAt();
+    }
+
+    public void toDelete() {
+        this.commentFlag = false;
+    }
 }
