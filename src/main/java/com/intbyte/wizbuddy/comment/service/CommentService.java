43e883a1a5cd81f0e2d233a49275657d2d9d8c0c
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-31 18:53:24 +0900

    ✨ 기능 추가 : Comment 등록 Service 기능 추가

--- src/main/java/com/intbyte/wizbuddy/comment/service/CommentService.java
+++ src/main/java/com/intbyte/wizbuddy/comment/service/CommentService.java
@@ -1,13 +1,17 @@
 package com.intbyte.wizbuddy.comment.service;
 
 
+import com.intbyte.wizbuddy.comment.domain.EditCommentInfo;
 import com.intbyte.wizbuddy.comment.domain.Entity.Comment;
 import com.intbyte.wizbuddy.comment.dto.CommentDTO;
+import com.intbyte.wizbuddy.comment.repository.CommentRepository;
 import com.intbyte.wizbuddy.exception.comment.CommentNotFoundException;
 import com.intbyte.wizbuddy.mapper.CommentMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +20,12 @@ public class CommentService {
 
     private final CommentMapper commentMapper;
     private final ModelMapper modelMapper;
+    private final CommentRepository commentRepository;
 
-    public CommentService(CommentMapper commentMapper, ModelMapper modelMapper) {
+    public CommentService(CommentMapper commentMapper, ModelMapper modelMapper,CommentRepository commentRepository) {
         this.commentMapper = commentMapper;
         this.modelMapper = modelMapper;
+        this.commentRepository = commentRepository;
     }
 
     public List<CommentDTO> findAllComment() {
@@ -31,12 +37,27 @@ public class CommentService {
 
     public CommentDTO findCommentById(int code) {
         Comment comment = commentMapper.selectCommentById(code);
-
         if(comment == null) {
             throw new CommentNotFoundException();
 
         }
         return modelMapper.map(comment, CommentDTO.class);
+    }
+
+    @Transactional
+    public void registerComment(CommentDTO comments) {
+
+        Comment comment = Comment.builder()
+                .commentContent(comments.getCommentContent())
+                .commentFlag(true)
+                .commentAdoptedState(false)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .subsCode(comments.getSubsCode())
+                .employeeCode(comments.getEmployeeCode())
+                .build();
+
+        commentRepository.save(comment);
     }
 
 
