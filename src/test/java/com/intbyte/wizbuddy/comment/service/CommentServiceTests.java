48e2aff7352284b8dadd8c214da68a8398f81f2c
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-31 15:50:00 +0900

    ✨ 기능 추가 : Comment 1개 조회 테스트 기능 추가

--- src/test/java/com/intbyte/wizbuddy/comment/service/CommentServiceTests.java
+++ src/test/java/com/intbyte/wizbuddy/comment/service/CommentServiceTests.java
@@ -2,11 +2,14 @@ package com.intbyte.wizbuddy.comment.service;
 
 import com.intbyte.wizbuddy.comment.dto.CommentDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CommentServiceTests {
@@ -15,12 +18,31 @@ class CommentServiceTests {
     private CommentService commentService;
 
     @Test
-    public void 댓글_전체_조회_테스트() {
+    @DisplayName("댓글_전체_조회_테스트")
+    public void findAllCommentTest() {
         Assertions.assertDoesNotThrow(
                 () -> {
                     List<CommentDTO> comments = commentService.findAllComment();
                     comments.forEach(System.out::println);
                 });
+
+    }
+
+    @Test
+    @DisplayName("댓글_1개_조회_테스트")
+    public void findCommentByIdTest(){
+
+        //given
+        int commentCode = 1;
+        
+        //when
+        CommentDTO comment = commentService.findCommentById(commentCode);
+        
+        //then
+        assertNotNull(comment);
+        assertNotNull(comment);
+        System.out.println("comment = " + comment);
+        
 
     }
 
