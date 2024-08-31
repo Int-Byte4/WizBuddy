132c4b408fbd535615d1d54e1a8fd9af84613751
YuJeeun <dbwpdms1122@naver.com>
2024-08-31 14:18:44 +0900

    ✨ 기능 추가: 직원별 근무일정 조회, 근무일 수정, 근무일 삭제 및 테스트 코드 추가

--- src/test/java/com/intbyte/wizbuddy/schedule/service/ScheduleServiceTests.java
+++ src/test/java/com/intbyte/wizbuddy/schedule/service/ScheduleServiceTests.java
@@ -1,5 +1,7 @@
 package com.intbyte.wizbuddy.schedule.service;
 
+import com.intbyte.wizbuddy.mapper.EmployeeWorkingPartMapper;
+import com.intbyte.wizbuddy.schedule.domain.EditScheduleInfo;
 import com.intbyte.wizbuddy.schedule.dto.EmployeeWorkingPartDTO;
 import com.intbyte.wizbuddy.schedule.dto.WeeklyScheduleDTO;
 import jakarta.transaction.Transactional;
@@ -21,6 +23,8 @@ class ScheduleServiceTests {
 
     @Autowired
     private ScheduleService scheduleService;
+    @Autowired
+    private EmployeeWorkingPartMapper employeeWorkingPartMapper;
 
 
     @Test
@@ -43,11 +47,51 @@ class ScheduleServiceTests {
     }
 
     @Test
+    @DisplayName("직원별 스케줄 조회 성공")
+    public void testScheduleService_select_employee() {
+        List<EmployeeWorkingPartDTO> employeeWorkPartList = scheduleService.findScheduleByEmployeeCode(8);
+
+        assertNotNull(employeeWorkPartList);
+        employeeWorkPartList.forEach(System.out::println);
+    }
+
+    @Test
     @DisplayName("스케줄 등록 성공")
     public void testScheduleService_insert() {
         WeeklyScheduleDTO weeklyScheduleDTOList = scheduleService.registWeeklySchedule(new WeeklyScheduleDTO(4, true, new Date(), LocalDateTime.now(), LocalDateTime.now()));
         assertNotNull(weeklyScheduleDTOList);
     }
+
+//    @Test
+//    @DisplayName("직원 근무배치 성공")
+//    public void testScheduleService_insert_employee() {
+//        EmployeeWorkingPartDTO employeeWorkingPartDTOList = scheduleService.registSchedulePerEmployee(new EmployeeWorkingPartDTO(19, 3, 2, LocalDateTime.now(), "1T"));
+//        assertNotNull(employeeWorkingPartDTOList);
+//    }
+
+    @Test
+    @DisplayName("스케줄 수정 성공")
+    public void testScheduleService_update() {
+        int employeeCode = 18;
+        EditScheduleInfo editScheduleInfo = new EditScheduleInfo(8, "1T");   // 코드, 파트타임이 들어감 employeeCode / workingDate / workingPartTime
+
+        scheduleService.EditSchedule(employeeCode, editScheduleInfo);
+
+        assertEquals(editScheduleInfo.getWorkingPartTime(),"1T");
+    }
+
+    @Test
+    @DisplayName("스케줄 삭제 성공")
+    public void testScheduleService_delete() {
+        int employeeCode = 18;
+        scheduleService.deleteSchedule(employeeCode);
+        List<EmployeeWorkingPartDTO> employeeWorkingPartDTO = scheduleService.findScheduleByEmployeeCode(18);
+        assertTrue(employeeWorkingPartDTO.isEmpty());
+    }
+
+
+
+
 
 
 
