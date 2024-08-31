6897b539257f5b1f88acc2e193df29f799e9fc40
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-31 18:58:38 +0900

    ✨ 기능 추가 : Comment 삭제 테스트 기능 추가

--- src/test/java/com/intbyte/wizbuddy/comment/service/CommentServiceTests.java
+++ src/test/java/com/intbyte/wizbuddy/comment/service/CommentServiceTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
