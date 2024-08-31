8a12f0fbaccaf8eef0241806ecdb83cc0297fef1
JoJeHuni <cho9759@gmail.com>
2024-08-31 23:44:23 +0900

    ✨ 기능 추가 : jwt 사용, passwordEncoder를 위해 추가

--- src/main/java/com/intbyte/wizbuddy/config/AppConfig.java
+++ src/main/java/com/intbyte/wizbuddy/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.intbyte.wizbuddy.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +12,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
