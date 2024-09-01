5cf23a3947c54a07c9ef834164c02a12c20b3990
Hellin22 <dno06117@naver.com>
2024-09-01 23:00:42 +0900

    ✨ 기능 추가 / 수정 task 엔티티 Shop 외래키 추가
    task 엔티티에 shop 컬럼이 생김에 따라 추가적인 서비스 코드 구현 및 테스트

--- src/main/java/com/intbyte/wizbuddy/task/domain/TaskMybatis.java
+++ src/main/java/com/intbyte/wizbuddy/task/domain/TaskMybatis.java
@@ -1,28 +1,21 @@
-package com.intbyte.wizbuddy.task.dto;
+package com.intbyte.wizbuddy.task.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-public class TaskDTO {
+public class TaskMybatis {
 
     private int taskCode;
-
     private String taskContents;
-
     private boolean taskFlag;
-
     private boolean taskFixedState;
-
     private LocalDateTime createdAt;
-
     private LocalDateTime updatedAt;
-
     private int shopCode;
 }
--- src/main/java/com/intbyte/wizbuddy/task/service/TaskService.java
+++ src/main/java/com/intbyte/wizbuddy/task/service/TaskService.java
@@ -1,13 +1,18 @@
 package com.intbyte.wizbuddy.task.service;
 
+import com.intbyte.wizbuddy.exception.shop.ShopNotFoundException;
 import com.intbyte.wizbuddy.exception.task.TaskNotFoundException;
 import com.intbyte.wizbuddy.mapper.TaskMapper;
+import com.intbyte.wizbuddy.shop.domain.entity.Shop;
+import com.intbyte.wizbuddy.shop.repository.ShopRepository;
 import com.intbyte.wizbuddy.task.domain.EditTaskInfo;
+import com.intbyte.wizbuddy.task.domain.TaskMybatis;
 import com.intbyte.wizbuddy.task.domain.entity.Task;
 import com.intbyte.wizbuddy.task.dto.TaskDTO;
 import com.intbyte.wizbuddy.task.repository.TaskRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,16 +26,20 @@ public class TaskService {
     private final TaskRepository taskRepository; // jpa
     private final TaskMapper taskMapper;        // mybatis
     private final ModelMapper modelMapper; // dto <-> entity 변환
+    private final ShopRepository shopRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository, TaskMapper mapper, ModelMapper modelMapper) {
+    public TaskService(TaskRepository taskRepository, TaskMapper mapper, ModelMapper modelMapper, @Qualifier("shopRepository") ShopRepository shopRepository) {
         this.taskRepository = taskRepository;
         this.taskMapper = mapper;
         this.modelMapper = modelMapper;
+        this.shopRepository = shopRepository;
     }
 
     @Transactional
     public void insertTask(TaskDTO taskInfo) {
+
+        Shop shop = shopRepository.findById(taskInfo.getShopCode()).orElseThrow(ShopNotFoundException::new);
 
         Task task = Task.builder()
                 .taskContents(taskInfo.getTaskContents())
@@ -38,6 +47,7 @@ public class TaskService {
                 .taskFixedState(taskInfo.isTaskFixedState())
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
+                .shop(shop)
                 .build();
 
         taskRepository.save(task);
@@ -46,14 +56,14 @@ public class TaskService {
     @Transactional
     public TaskDTO findTaskById(int taskCode){
 
-        Task findTask = taskMapper.findTaskById(taskCode);
+        TaskMybatis findTask = taskMapper.findTaskById(taskCode);
         return modelMapper.map(findTask, TaskDTO.class);
     }
 
     @Transactional
     public List<TaskDTO> findAllTask(){
 
-        List<Task> findTasks = taskMapper.findAllTask();
+        List<TaskMybatis> findTasks = taskMapper.findAllTask();
 
         return findTasks.stream()
                 .map(task -> modelMapper.map(task, TaskDTO.class))
@@ -63,7 +73,7 @@ public class TaskService {
     @Transactional
     public List<TaskDTO> findAllTaskByFixedState(){
 
-        List<Task> findTasks = taskMapper.findAllTaskByFixedState();
+        List<TaskMybatis> findTasks = taskMapper.findAllTaskByFixedState();
 
         return findTasks.stream()
                 .map(task -> modelMapper.map(task, TaskDTO.class))
@@ -73,13 +83,51 @@ public class TaskService {
     @Transactional
     public List<TaskDTO> findAllTasksByTaskFlag(){
 
-        List<Task> findTasks = taskMapper.findAllTasksByTaskFlag();
+        List<TaskMybatis> findTasks = taskMapper.findAllTasksByTaskFlag();
+
+        return findTasks.stream()
+                .map(task -> modelMapper.map(task, TaskDTO.class))
+                .collect(Collectors.toList());
+    }
+    // ---------- 관리자 입장에서 사용하는 기능(매장 상관없이 모든 업무 조회) -------------
+
+
+    // 특정 매장의 모든 task 조회
+    @Transactional
+    public List<TaskDTO> findAllTaskByShopCode(int shopCode){
+        List<TaskMybatis> findTasks = taskMapper.findAllTaskByShopCode(shopCode);
 
         return findTasks.stream()
                 .map(task -> modelMapper.map(task, TaskDTO.class))
                 .collect(Collectors.toList());
     }
 
+    // 특정 매장의 고정 task 보기
+    @Transactional
+    public List<TaskDTO> findAllTaskByShopCodeByFixedState(int shopCode){
+        List<TaskMybatis> findTasksFixedState = taskMapper.findAllTaskByShopCodeByFixedState(shopCode);
+
+        return findTasksFixedState.stream()
+                .map(task -> modelMapper.map(task, TaskDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    // 특정 매장의 고정 아닌 task 보기
+    @Transactional
+    public List<TaskDTO> findAllTaskByShopCodeByNonFixedState(int shopCode){
+        List<TaskMybatis> findTasks = taskMapper.findAllTaskByShopCodeByNonFixedState(shopCode);
+
+        return findTasks.stream()
+                .map(task -> modelMapper.map(task, TaskDTO.class))
+                .collect(Collectors.toList());
+    }
+    // 이 2개의 기능은 동적 쿼리를 통해 하나로 줄일 수 있을듯?
+    // ---------- 매장 관리자(사장) 입장에서 사용하는 조회 기능 -------------
+
+
+
+    // 특정 매장의 수정, 삭제 기능 필요 -> 필요 없을거 같음. 이미 사장이 보는 task는 본인 매장의 task이기 때문에
+    // task 번호만 알면 그걸 삭제하거나 수정하면됨 !!
     @Transactional
     public void modifyTask(int taskCode, EditTaskInfo modifyTaskInfo){
 
@@ -92,7 +140,7 @@ public class TaskService {
     @Transactional
     public void deleteTask(int taskCode){
 
-        Task task = taskMapper.findTaskById(taskCode);
+        Task task = taskRepository.findById(taskCode).orElseThrow(TaskNotFoundException::new);
         EditTaskInfo editTaskInfo = new EditTaskInfo(task.getTaskContents(), false, task.isTaskFixedState(), task.getUpdatedAt());
 
         task.modify(editTaskInfo);
