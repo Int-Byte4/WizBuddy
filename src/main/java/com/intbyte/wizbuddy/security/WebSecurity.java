0b053d341d5bc52d81952aaccf68b138cf8fc7da
seohyun <694090@gmail.com>
2024-09-03 10:29:04 +0900

    ✨ 기능 추가: 매뉴얼 게시판 전체 조회 controller, vo 추가

--- src/main/java/com/intbyte/wizbuddy/security/WebSecurity.java
+++ src/main/java/com/intbyte/wizbuddy/security/WebSecurity.java
@@ -58,6 +58,7 @@ public class WebSecurity {
                                 .requestMatchers(new AntPathRequestMatcher("/shop/*/task", "GET")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/task", "POST")).hasRole("EMPLOYER")
                                 .requestMatchers(new AntPathRequestMatcher("/shop/*/task/*", "POST")).hasRole("EMPLOYER")
+                                .requestMatchers(new AntPathRequestMatcher("/manualBoard", "GET")).hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 /* 설명. authenticationManager 등록(UserDetails를 상속받는 Service 계층 + BCrypt 암호화) */
