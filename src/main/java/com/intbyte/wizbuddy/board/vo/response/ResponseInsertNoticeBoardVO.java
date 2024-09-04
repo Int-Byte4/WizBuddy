4a6a633bc8e02505111518a3d2baef05bd18e1b9
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-04 12:13:01 +0900

    ✨ 기능 추가 : 공지사항, 매뉴얼 게시글 조회, 등록, 수정, 삭제 api 요청 및 응답 VO 기능 추가

---
+++ src/main/java/com/intbyte/wizbuddy/board/vo/response/ResponseInsertNoticeBoardVO.java
@@ -0,0 +1,24 @@
+package com.intbyte.wizbuddy.board.vo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class ResponseInsertNoticeBoardVO {
+    private int noticeCode;
+    private String noticeTitle;
+    private String noticeContent;
+    private boolean noticeFlag;
+    private String imageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private int shopCode;
+    private String employerCode;
+}
