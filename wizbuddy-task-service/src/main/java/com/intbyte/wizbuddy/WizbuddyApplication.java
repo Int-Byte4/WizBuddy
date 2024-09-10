3b6d445a76a89ad0844e311758cc20462ea06298
Hellin22 <dno06117@naver.com>
2024-09-10 19:46:19 +0900

    ✨ 기능 추가 / 수정: client로 추가하기 위한 @EnableFeignClients 추가

--- wizbuddy-task-service/src/main/java/com/intbyte/wizbuddy/WizbuddyApplication.java
+++ wizbuddy-task-service/src/main/java/com/intbyte/wizbuddy/WizbuddyApplication.java
@@ -2,8 +2,10 @@ package com.intbyte.wizbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WizbuddyApplication {
 
     public static void main(String[] args) {
