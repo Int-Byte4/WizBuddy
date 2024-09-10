07c2725fceb47f33cf755d3c8c1648a5964ea716
seohyun <694090@gmail.com>
2024-09-10 20:59:04 +0900

    ✨ 기능 추가: 매장 swagger 추가

--- wizbuddy-shop-service/src/main/java/com/intbyte/wizbuddy/employeepershop/config/SwaggerConfig.java
+++ wizbuddy-shop-service/src/main/java/com/intbyte/wizbuddy/employeepershop/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.intbyte.wizbuddy.board.config;
+package com.intbyte.wizbuddy.employeepershop.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Configuration;
 
 /* 설명. http://localhost:8080/swagger-ui/index.html */
 @OpenAPIDefinition(
-        info = @Info(title = "WizBuddy 게시판 API 명세서",
+        info = @Info(title = "WizBuddy 매장 별 직원 API 명세서",
                 description = "WizBuddy 기능별 API 명세서"))
-@Configuration("BoardSwaggerConfiguration")
+@Configuration("EmployeePerShopSwaggerConfiguration")
 public class SwaggerConfig {
 
 }
