cd3aee81e3bb3352678f75a97e5519acca568739
Hellin22 <dno06117@naver.com>
2024-09-11 18:33:00 +0900

    ✨ 기능 추가 / 수정: TaskService(query) 테스트 완료

--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/task/command/application/service/TaskServiceTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/task/command/application/service/TaskServiceTests.java
@@ -8,112 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
-
 @SpringBootTest
 class TaskServiceTests {
 
     @Autowired
     private AppTaskService taskService;
-
-
-//    @Test
-//    @Transactional
-//    public void 전체_업무_조회_테스트(){
-//        List<TaskMybatis> allTask = taskMapper.findAllTask();
-//        for (int i = 0; i < allTask.size(); i++) {
-//            assertNotNull(allTask);
-//            System.out.println(allTask.get(i));
-//        }
-//    }
-//
-//
-//    @Test
-//    @Transactional
-//    public void 고정된_전체_업무_조회_테스트(){
-//        List<TaskMybatis> allTask = taskMapper.findAllTaskByFixedState();
-//        for (int i = 0; i < allTask.size(); i++) {
-//            assertNotNull(allTask);
-//            System.out.println(allTask.get(i));
-//        }
-//    }
-//
-//
-//    @Test
-//    @Transactional
-//    public void 삭제되지않은_전체_업무_조회_테스트(){
-//        List<TaskMybatis> allTask = taskMapper.findAllTasksByTaskFlag();
-//        for (int i = 0; i < allTask.size(); i++) {
-//            assertNotNull(allTask);
-//            System.out.println(allTask.get(i));
-//        }
-//    }
-//
-//    @Test
-//    @Transactional // 1번
-//    public void id로_업무_1개_조회_테스트(){
-//
-//        // given
-//        int taskCode = taskMapper.findAllTask().size() - 1;
-//
-//        // when
-//        TaskQueryDTO task = taskService.findTaskById(taskCode);
-//
-//        // then
-//        assertNotNull(task);
-//    }
-//
-//    // 2-1번
-//    @Test
-//    @Transactional
-//    public void 매장_id로_모든_업무_조회(){
-//
-//        // given
-//        int shopCode = 1;
-//
-//        // when
-//        List<TaskQueryDTO> allTaskByShopCode = taskService.findAllTaskByShopCode(shopCode);
-//
-//        // then
-//        for (int i = 0; i < allTaskByShopCode.size(); i++) {
-//            System.out.println(allTaskByShopCode.get(i));
-//            assertNotNull(allTaskByShopCode.get(i));
-//        }
-//    }
-//
-//    // 2-2번
-//    @Test
-//    @Transactional
-//    public void 매장_id로_고정된_모든_업무_조회(){
-//        // given
-//        int shopCode = 1;
-//
-//        // when
-//        List<TaskQueryDTO> allTaskByShopCode = taskService.findAllTaskByShopCodeByFixedState(shopCode);
-//
-//        // then
-//        for (int i = 0; i < allTaskByShopCode.size(); i++) {
-//            System.out.println(allTaskByShopCode.get(i));
-//            assertNotNull(allTaskByShopCode.get(i));
-//        }
-//    }
-//
-//    // 2-3번
-//    @Test
-//    @Transactional
-//    public void 매장_id로_고정_안된_모든_업무_조회(){
-//        // given
-//        int shopCode = 2;
-//
-//        // when
-//        List<TaskQueryDTO> allTaskByShopCode = taskService.findAllTaskByShopCodeByNonFixedState(shopCode);
-//
-//        // then
-//        for (int i = 0; i < allTaskByShopCode.size(); i++) {
-//            System.out.println(allTaskByShopCode.get(i));
-//            assertNotNull(allTaskByShopCode.get(i));
-//        }
-//    }
-
 
     // 3. 특정 매장에 1개 task 추가
     @Test
--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/task/query/service/TaskServicesTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/task/query/service/TaskServicesTests.java
@@ -1,4 +1,57 @@
 package com.intbyte.wizbuddy.task.query.service;
 
+import com.intbyte.wizbuddy.task.query.dto.TaskQueryDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
 public class TaskServicesTests {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    @Transactional // 1번
+    public void id로_업무_1개_조회_테스트(){
+
+        int taskCode = 1;
+
+        TaskQueryDTO taskById = taskService.findTaskById(taskCode);
+        System.out.println("taskById = " + taskById);
+    }
+
+    // 2-1번
+    @Test
+    @Transactional
+    public void 매장_id로_모든_업무_조회(){
+
+        int shopCode = 1;
+
+        List<TaskQueryDTO> allTaskByShopCode = taskService.findAllTaskByShopCode(shopCode);
+        System.out.println(allTaskByShopCode.toString());
+    }
+
+    // 2-2번
+    @Test
+    @Transactional
+    public void 매장_id로_고정된_모든_업무_조회(){
+
+        int shopCode = 1;
+        List<TaskQueryDTO> allTask = taskService.findAllTaskByShopCodeByFixedState(shopCode);
+        System.out.println(allTask.toString());
+    }
+
+    // 2-3번
+    @Test
+    @Transactional
+    public void 매장_id로_고정_안된_모든_업무_조회(){
+
+        int shopCode = 1;
+        List<TaskQueryDTO> allTask = taskService.findAllTaskByShopCodeByNonFixedState(shopCode);
+        System.out.println(allTask.toString());
+    }
 }
