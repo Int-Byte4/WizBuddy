fba295ed309d7dd412302cd46ec1f1527682c6f6
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-09-11 13:37:53 +0900

    ♻️ 코드 수정: UserServiceImpl 코드 수정

--- wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/application/service/UserServiceImpl.java
+++ wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/application/service/UserServiceImpl.java
@@ -2,24 +2,23 @@ package com.intbyte.wizbuddy.user.command.application.service;
 
 import com.intbyte.wizbuddy.user.common.exception.CommonException;
 import com.intbyte.wizbuddy.user.common.exception.StatusEnum;
-import com.intbyte.wizbuddy.user.command.domain.entity.Employee;
-import com.intbyte.wizbuddy.user.command.domain.entity.Employer;
-import com.intbyte.wizbuddy.user.command.domain.entity.UserEntity;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.Employee;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.Employer;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.UserEntity;
 import com.intbyte.wizbuddy.user.command.domain.repository.EmployeeRepository;
 import com.intbyte.wizbuddy.user.command.domain.repository.EmployerRepository;
 import com.intbyte.wizbuddy.user.command.domain.repository.UserRepository;
-import com.intbyte.wizbuddy.user.command.domain.entity.vo.request.RequestRegisterEmployeeVO;
-import com.intbyte.wizbuddy.user.command.domain.entity.vo.request.RequestRegisterEmployerVO;
-import com.intbyte.wizbuddy.user.command.domain.entity.vo.request.RequestSignInUserVO;
-import com.intbyte.wizbuddy.user.command.domain.entity.vo.response.ResponseInsertEmployeeVO;
-import com.intbyte.wizbuddy.user.command.domain.entity.vo.response.ResponseInsertEmployerVO;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.vo.request.RequestRegisterEmployeeVO;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.vo.request.RequestRegisterEmployerVO;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.vo.request.RequestSignInUserVO;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.vo.response.ResponseInsertEmployeeVO;
+import com.intbyte.wizbuddy.user.command.domain.aggregate.vo.response.ResponseInsertEmployerVO;
 import com.intbyte.wizbuddy.user.query.dto.EmployeeDTO;
 import com.intbyte.wizbuddy.user.query.dto.EmployerDTO;
 import com.intbyte.wizbuddy.user.query.dto.UserDTO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -46,24 +45,21 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     @Override
-    public ResponseInsertEmployerVO signInEmployer(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployerVO registerEmployerInfo) {
+    public ResponseInsertEmployerVO signInEmployer(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployerVO requestRegisterEmployerVO) {
         String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         String uuid = UUID.randomUUID().toString();
         String customUserCode = currentDate + uuid.substring(8);
 
-        requestSignInUserVO.setUserCode(customUserCode);
-
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
         if (userRepository.findByUserEmail(requestSignInUserVO.getUserEmail()) != null) throw new CommonException(StatusEnum.EMAIL_DUPLICATE);
 
         requestSignInUserVO.setUserPassword(bCryptPasswordEncoder.encode(requestSignInUserVO.getUserPassword()));
-        registerEmployerInfo.setEmployerPassword(bCryptPasswordEncoder.encode(registerEmployerInfo.getEmployerPassword()));
+        requestRegisterEmployerVO.setEmployerPassword(bCryptPasswordEncoder.encode(requestRegisterEmployerVO.getEmployerPassword()));
 
         if (requestSignInUserVO.getUserType().equals("EMPLOYER")) requestSignInUserVO.setUserType("Employer");
 
         UserDTO userDTO = userDTOBuild(requestSignInUserVO);
-        EmployerDTO employerQueryDTO = employerDTOBuild(requestSignInUserVO, registerEmployerInfo);
+        userDTO.setUserCode(customUserCode);
+        EmployerDTO employerQueryDTO = employerDTOBuild(userDTO, requestRegisterEmployerVO);
 
         UserEntity userEntity = modelMapper.map(userDTO, UserEntity.class);
         Employer employer = modelMapper.map(employerQueryDTO, Employer.class);
@@ -71,29 +67,26 @@ public class UserServiceImpl implements UserService {
         userRepository.save(userEntity);
         employerRepository.save(employer);
 
-        return new ResponseInsertEmployerVO(requestSignInUserVO, registerEmployerInfo);
+        return new ResponseInsertEmployerVO(userDTO, employerQueryDTO);
     }
 
     @Transactional
     @Override
-    public ResponseInsertEmployeeVO signInEmployee(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployeeVO registerEmployeeInfo) {
+    public ResponseInsertEmployeeVO signInEmployee(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployeeVO requestRegisterEmployeeVO) {
         String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         String uuid = UUID.randomUUID().toString();
         String customUserCode = currentDate + uuid.substring(8);
 
-        requestSignInUserVO.setUserCode(customUserCode);
-
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
         if (userRepository.findByUserEmail(requestSignInUserVO.getUserEmail()) != null) throw new CommonException(StatusEnum.EMAIL_DUPLICATE);
 
         requestSignInUserVO.setUserPassword(bCryptPasswordEncoder.encode(requestSignInUserVO.getUserPassword()));
-        registerEmployeeInfo.setEmployeePassword(bCryptPasswordEncoder.encode(registerEmployeeInfo.getEmployeePassword()));
+        requestRegisterEmployeeVO.setEmployeePassword(bCryptPasswordEncoder.encode(requestRegisterEmployeeVO.getEmployeePassword()));
 
         if (requestSignInUserVO.getUserType().equals("EMPLOYEE")) requestSignInUserVO.setUserType("Employee");
 
         UserDTO userDTO = userDTOBuild(requestSignInUserVO);
-        EmployeeDTO employeeDTO = employeeDTOBuild(requestSignInUserVO, registerEmployeeInfo);
+        userDTO.setUserCode(customUserCode);
+        EmployeeDTO employeeDTO = employeeDTOBuild(userDTO, requestRegisterEmployeeVO);
 
         UserEntity userEntity = modelMapper.map(userDTO, UserEntity.class);
         Employee employee = modelMapper.map(employeeDTO, Employee.class);
@@ -101,7 +94,7 @@ public class UserServiceImpl implements UserService {
         userRepository.save(userEntity);
         employeeRepository.save(employee);
 
-        return new ResponseInsertEmployeeVO(requestSignInUserVO, registerEmployeeInfo);
+        return new ResponseInsertEmployeeVO(userDTO, employeeDTO);
     }
 
     @Override
@@ -127,19 +120,18 @@ public class UserServiceImpl implements UserService {
 
     private UserDTO userDTOBuild(RequestSignInUserVO requestSignInUserVO) {
         return UserDTO.builder()
-                .userCode(requestSignInUserVO.getUserCode())
+                .userType(requestSignInUserVO.getUserType())
                 .userEmail(requestSignInUserVO.getUserEmail())
                 .userPassword(requestSignInUserVO.getUserPassword())
-                .userType(requestSignInUserVO.getUserType())
                 .build();
     }
 
-    private EmployerDTO employerDTOBuild(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployerVO requestRegisterEmployerVO) {
+    private EmployerDTO employerDTOBuild(UserDTO userDTO, RequestRegisterEmployerVO requestRegisterEmployerVO) {
         return EmployerDTO.builder()
-                .employerCode(requestSignInUserVO.getUserCode())
+                .employerCode(userDTO.getUserCode())
                 .employerName(requestRegisterEmployerVO.getEmployerName())
-                .employerEmail(requestSignInUserVO.getUserEmail())
-                .employerPassword(requestSignInUserVO.getUserPassword())
+                .employerEmail(userDTO.getUserEmail())
+                .employerPassword(userDTO.getUserPassword())
                 .employerPhone(requestRegisterEmployerVO.getEmployerPhone())
                 .employerFlag(true)
                 .employerBlackState(false)
@@ -148,12 +140,12 @@ public class UserServiceImpl implements UserService {
                 .build();
     }
 
-    private EmployeeDTO employeeDTOBuild(RequestSignInUserVO requestSignInUserVO, RequestRegisterEmployeeVO requestRegisterEmployeeVO) {
+    private EmployeeDTO employeeDTOBuild(UserDTO userDTO, RequestRegisterEmployeeVO requestRegisterEmployeeVO) {
         return EmployeeDTO.builder()
-                .employeeCode(requestSignInUserVO.getUserCode())
+                .employeeCode(userDTO.getUserCode())
                 .employeeName(requestRegisterEmployeeVO.getEmployeeName())
-                .employeeEmail(requestSignInUserVO.getUserEmail())
-                .employeePassword(requestSignInUserVO.getUserPassword())
+                .employeeEmail(userDTO.getUserEmail())
+                .employeePassword(userDTO.getUserPassword())
                 .employeePhone(requestRegisterEmployeeVO.getEmployeePhone())
                 .employeeFlag(true)
                 .latitude(null)
