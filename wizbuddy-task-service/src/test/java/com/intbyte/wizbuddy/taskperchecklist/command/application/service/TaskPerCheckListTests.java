66e14078a71f22b5058cc5e9e48810d95f4350e2
Hellin22 <dno06117@naver.com>
2024-09-12 10:23:06 +0900

    ✨ 기능 추가: command, query TaskPerCheckListTests 수정 완료

--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/taskperchecklist/command/application/service/TaskPerCheckListTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/taskperchecklist/command/application/service/TaskPerCheckListTests.java
@@ -1,24 +1,27 @@
 package com.intbyte.wizbuddy.taskperchecklist.command.application.service;
 
 import com.intbyte.wizbuddy.taskperchecklist.command.application.dto.TaskPerCheckListDTO;
-import com.intbyte.wizbuddy.taskperchecklist.query.service.TaskPerCheckListService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
+@Transactional
 public class TaskPerCheckListTests {
 
     @Autowired
     private AppTaskPerCheckListService taskPerCheckListService;
 
-    // 4. 특정 매장(특정 체크리스트)의 특정 업무 finished 테스트
+    // 특정 매장(특정 체크리스트)의 특정 업무 finished 테스트
     @Test
     @DisplayName("특정 매장(특정 체크리스트)의 특정 업무 finished 테스트")
-    public void modifyTaskPerCheckList(){
+    public void modifyTaskPerCheckList() {
 
         int checkListCode = 1;
         int taskCode = 2;
@@ -30,6 +33,8 @@ public class TaskPerCheckListTests {
         dto.setUpdatedAt(LocalDateTime.now());
         dto.setEmployeeCode("20240831-1859-4c43-b692-b6cb5891c24a");
 
-        taskPerCheckListService.modifyTaskPerCheckList(dto);
+        assertDoesNotThrow(() -> {
+            taskPerCheckListService.modifyTaskPerCheckList(dto);
+        });
     }
 }
--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/taskperchecklist/query/service/TaskPerCheckListTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/taskperchecklist/query/service/TaskPerCheckListTests.java
@@ -1,10 +1,14 @@
 package com.intbyte.wizbuddy.taskperchecklist.query.service;
 
+import com.intbyte.wizbuddy.taskperchecklist.query.dto.TaskPerCheckListQueryDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TaskPerCheckListTests {
@@ -15,33 +19,36 @@ public class TaskPerCheckListTests {
     // 1-1.
     @Test
     @DisplayName("특정 체크리스트속 업무 조회")
-    @Transactional
     public void findAllTaskPerCheckListByCheckListCodeTest(){
 
         int checkListCode = 1;
+        List<TaskPerCheckListQueryDTO> tpcDTOs = taskPerCheckListService.findAllTaskPerCheckListByCheckListCode(checkListCode);
 
-        taskPerCheckListService.findAllTaskPerCheckListByCheckListCode(checkListCode);
+        for (int i = 0; i < tpcDTOs.size(); i++)
+            assertEquals(checkListCode, tpcDTOs.get(i).getCheckListCode());
     }
 
     // 1-2.
     @Test
     @DisplayName("특정 체크리스트속 완료된 업무 조회")
-    @Transactional
     public void findAllTaskPerCheckListByCheckListCodeFinishedTest(){
 
         int checkListCode = 1;
+        List<TaskPerCheckListQueryDTO> finishedDTOs = taskPerCheckListService.findAllTaskPerCheckListByCheckListCodeByFinished(checkListCode);
 
-        taskPerCheckListService.findAllTaskPerCheckListByCheckListCodeByFinished(checkListCode);
+        for (int i = 0; i < finishedDTOs.size(); i++)
+            assertEquals(true, finishedDTOs.get(i).getTaskFinishedState());
     }
 
     // 1-3.
     @Test
     @DisplayName("특정 체크리스트속 미완료된 업무 조회")
-    @Transactional
     public void findAllTaskPerCheckListByCheckListCodeNotFinishedTest() {
 
         int checkListCode = 1;
+        List<TaskPerCheckListQueryDTO> nonFinishedDTOs = taskPerCheckListService.findAllTaskPerCheckListByCheckListCodeByNotFinished(checkListCode);
 
-        taskPerCheckListService.findAllTaskPerCheckListByCheckListCodeByNotFinished(checkListCode);
+        for (int i = 0; i < nonFinishedDTOs.size(); i++)
+            assertEquals(false, nonFinishedDTOs.get(i).getTaskFinishedState());
     }
 }
