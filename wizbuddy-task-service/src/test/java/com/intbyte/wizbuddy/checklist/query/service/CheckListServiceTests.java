8b90785d2222804467f538e0c5d0d8dd09fd7fb6
Hellin22 <dno06117@naver.com>
2024-09-12 10:03:13 +0900

    ✨ 기능 추가: CheckListServiceTests query 최종 수정

--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/checklist/query/service/CheckListServiceTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/checklist/query/service/CheckListServiceTests.java
@@ -1,5 +1,6 @@
 package com.intbyte.wizbuddy.checklist.query.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +8,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class CheckListServiceTests {
 
     @Autowired
     private CheckListService checkListService;
 
     @Test
-    @DisplayName("어떤 매장의 true인 모든 checklist 가져오기")
-    @Transactional
+    @DisplayName("어떤 매장의 true인 모든 체크리스트 가져오기")
     public void findAllCheckListTest(){
+
         int shopCode = 1;
 
-        checkListService.findCheckListByIdByShop(shopCode);
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.findCheckListByIdByShop(shopCode);
+        });
     }
 
     @Test
     @DisplayName("체크리스트 1개 조회 테스트")
-    @Transactional
     public void findCheckListByIdTest(){
+
         int checkListCode = 1;
-        checkListService.findCheckListById(checkListCode);
+
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.findCheckListById(checkListCode);
+        });
     }
 }
