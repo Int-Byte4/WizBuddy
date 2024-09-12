dcf47dee5fd3ae33daa1d3442ffb85800a9be401
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-12 09:47:20 +0900

    ✨ 기능 수정: 댓글 채택 및 스케줄 코드 업데이트 기능 수정

--- wizbuddy-schedule-service/src/main/java/com/intbyte/wizbuddy/comment/infrastructure/service/InfraAdoptServiceImpl.java
+++ wizbuddy-schedule-service/src/main/java/com/intbyte/wizbuddy/comment/infrastructure/service/InfraAdoptServiceImpl.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service("InfraAdoptService")
 @RequiredArgsConstructor
