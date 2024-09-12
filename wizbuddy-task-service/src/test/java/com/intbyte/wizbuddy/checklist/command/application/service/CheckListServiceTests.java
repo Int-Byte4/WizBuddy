70fa974a663aa61f5f64dd4357f070f59683f132
Hellin22 <dno06117@naver.com>
2024-09-12 09:49:07 +0900

    ✨ 기능 수정: CheckListServiceTests 최종 수정 완료

--- wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/checklist/command/application/service/CheckListServiceTests.java
+++ wizbuddy-task-service/src/test/java/com/intbyte/wizbuddy/checklist/command/application/service/CheckListServiceTests.java
@@ -2,22 +2,68 @@ package com.intbyte.wizbuddy.checklist.command.application.service;
 
 
 import com.intbyte.wizbuddy.checklist.command.application.dto.CheckListDTO;
+import com.intbyte.wizbuddy.common.exception.CommonException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@Transactional
 class CheckListServiceTests {
 
     @Autowired
     private AppCheckListService checkListService;
 
+    // 특정 매장에 체크리스트 1개 등록시 해당 매장의 fixed task 자동 추가 테스트
+    @Test
+    @DisplayName("체크리스트 생성시 해당 매장의 fixed task 자동 추가 테스트")
+    public void insertCheckListTest(){
+
+        int shopCode = 1;
+
+        CheckListDTO dto = new CheckListDTO();
+        dto.setCheckListTitle("새로운 체크리스트가 등록되고 있어용");
+        dto.setCheckListFlag(true);
+        dto.setCreatedAt(LocalDateTime.now());
+        dto.setUpdatedAt(LocalDateTime.now());
+        dto.setShopCode(shopCode);
+
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.insertCheckList(dto);
+        });
+    }
+
+    // 특정 매장에 체크리스트 1개 등록
+    @Test
+    @DisplayName("체크리스트에 업무 등록 테스트")
+    public void insertTaskToCheckListTest(){
+
+        int checkListCode = 1;
+        int taskCode = 1;
+
+        Assertions.assertThrows(CommonException.class, () -> {
+            checkListService.insertTaskToCheckList(checkListCode, taskCode);
+        });
+    }
 
     @Test
-    @DisplayName("체크리스트 수정 성공")
+    @DisplayName("체크리스트 업무 삭제 테스트")
+    public void deleteTaskFromCheckListTest(){
+        int checkListCode = 1;
+        int taskCode = 1;
+
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.deleteTaskFromCheckList(checkListCode, taskCode);
+        });
+    }
+
+    @Test
+    @DisplayName("체크리스트 자체 수정 테스트")
     public void modifyCheckListTest(){
 
         int checkListCode = 1;
@@ -27,44 +73,19 @@ class CheckListServiceTests {
         checkListDTO.setCheckListFlag(true);
         checkListDTO.setUpdatedAt(LocalDateTime.now());
 
-        checkListService.modifyCheckList(checkListCode,checkListDTO);
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.modifyCheckList(checkListCode, checkListDTO);
+        });
     }
 
     @Test
-    @DisplayName("체크리스트 삭제 성공")
-    public void deleteCheckListTest(){
+    @DisplayName("체크리스트 삭제 테스트")
+    public void deleteCheckListTest() {
 
         int checkListCode = 1;
-        checkListService.deleteCheckList(checkListCode);
-    }
 
-
-
-    // 3-1.특정 매장에 체크리스트 1개 등록
-    @Test
-    @DisplayName("특정 매장에 체크리스트 1개 등록 성공")
-    public void insertCheckListTest(){
-        // given:
-        int checkListCode = 1;
-        int shopCode = 1;
-
-        checkListService.insertTaskToCheckList(checkListCode, shopCode);
-    }
-
-    // 3-2. 특정 매장에 체크리스트 1개 등록시 해당 매장의 fixed task 자동 추가 테스트
-    @Test
-    @DisplayName("특정 매장에 체크리스트 생성시 해당 매장의 fixed task 자동 추가 테스트")
-    public void insertCheckListFixedStateTask(){
-
-        int shopCode = 1;
-
-        CheckListDTO dto = new CheckListDTO();
-        dto.setCheckListTitle("3번 체크리스트에는 2번 매장");
-        dto.setCheckListFlag(true);
-        dto.setCreatedAt(LocalDateTime.now());
-        dto.setUpdatedAt(LocalDateTime.now());
-        dto.setShopCode(shopCode);
-
-        checkListService.insertCheckList(dto);
+        Assertions.assertDoesNotThrow(() -> {
+            checkListService.deleteCheckList(checkListCode);
+        });
     }
 }
