0bf5cb07db34ca31c803855d79cccb44d9188df0
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-13 10:08:44 +0900

    ✨ 기능 추가: 게시글 삭제시 관련 자동 댓글 삭제 관련 순환참조 방지 SubsBoardEventListener 및 Event 객체 추가

--- wizbuddy-schedule-service/src/test/java/com/intbyte/wizbuddy/comment/command/application/service/CommentServiceImplTests.java
+++ wizbuddy-schedule-service/src/test/java/com/intbyte/wizbuddy/comment/command/application/service/CommentServiceImplTests.java
@@ -1,11 +1,9 @@
 package com.intbyte.wizbuddy.comment.command.application.service;
 
-import com.intbyte.wizbuddy.board.command.domain.aggregate.SubsBoard;
 import com.intbyte.wizbuddy.comment.command.application.dto.CommentDTO;
 import com.intbyte.wizbuddy.comment.command.domain.aggregate.Comment;
 import com.intbyte.wizbuddy.comment.command.domain.aggregate.vo.EditCommentVO;
 import com.intbyte.wizbuddy.comment.command.domain.repository.CommentRepository;
-import com.intbyte.wizbuddy.employeeworkingpart.command.domain.aggregate.entity.EmployeeWorkingPart;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
