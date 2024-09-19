0ad8f312e27430ec8b5862b4778178a9f1577456
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-09-19 12:32:05 +0900

    ♻️ 코드 수정: User에 컬럼 추가 및 EmployeeAdditional 추가로 인한 DTO, VO 변동

--- wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/application/dto/RequestEditUserDTO.java
+++ wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/application/dto/RequestEditUserDTO.java
@@ -2,16 +2,14 @@ package com.intbyte.wizbuddy.user.command.application.dto;
 
 import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 @Builder
-public class RequestEditEmployeeDTO {
-    private String employeePassword;
-    private String employeePhone;
-    private LocalDate employeeHealthDate;
+public class RequestEditUserDTO {
+    private String userPassword;
+    private String userPhone;
     private LocalDateTime updatedAt;
 }
--- wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/domain/aggregate/vo/request/RequestRegisterUserVO.java
+++ wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/command/domain/aggregate/vo/request/RequestRegisterUserVO.java
@@ -1,17 +1,17 @@
-package com.intbyte.wizbuddy.user.query.dto;
+package com.intbyte.wizbuddy.user.command.domain.aggregate.vo.request;
 
 import com.intbyte.wizbuddy.user.command.domain.aggregate.UserTypeEnum;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
-@Builder
-@ToString
-public class UserDTO {
-    private String userCode;
+public class RequestRegisterUserVO {
     private UserTypeEnum userType;
     private String userName;
     private String userEmail;
