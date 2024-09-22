85e74968b8dee6d32444662d75454d9eff5ac749
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-09-23 01:27:32 +0900

    ✨ 기능 추가: DTO에 @JsonProperty 어노테이션 추가 + UserEntity에 kakaoId 컬럼 추가

--- wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/query/dto/UserDTO.java
+++ wizbuddy-user-service/src/main/java/com/intbyte/wizbuddy/user/query/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.intbyte.wizbuddy.user.query.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.intbyte.wizbuddy.user.command.domain.aggregate.UserTypeEnum;
 import lombok.*;
 
@@ -11,14 +12,42 @@ import java.time.LocalDateTime;
 @Builder
 @ToString
 public class UserDTO {
+    @JsonProperty("user_code")
     private String userCode;
+
+    @JsonProperty("user_type")
     private UserTypeEnum userType;
+
+    @JsonProperty("user_name")
     private String userName;
+
+    @JsonProperty("user_email")
     private String userEmail;
+
+    @JsonProperty("user_password")
     private String userPassword;
+
+    @JsonProperty("user_phone")
     private String userPhone;
+
+    @JsonProperty("user_flag")
     private boolean userFlag;
+
+    @JsonProperty("user_black_state")
     private boolean userBlackState;
+
+    @JsonProperty("user_created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("user_updated_at")
     private LocalDateTime updatedAt;
+
+    @JsonProperty("user_kakao_id")
+    private String kakaoId;
+
+    @JsonProperty("user_kakao_access_token")
+    private String kakaoAccessToken;
+
+    @JsonProperty("user_jwt_token")
+    private String jwtToken;
 }
