40662a8ecef4b0bd811821ebd567b9dc5d943733
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-24 17:42:24 +0900

    ✨ 기능 수정/추가: FeignClient 기능 수정 및 추가

--- wizbuddy-schedule-service/src/main/java/com/intbyte/wizbuddy/employeeworkingpart/command/infrastructure/service/ScheduleInfraServiceImpl.java
+++ wizbuddy-schedule-service/src/main/java/com/intbyte/wizbuddy/employeeworkingpart/command/infrastructure/service/ScheduleInfraServiceImpl.java
@@ -1,28 +1,28 @@
-//package com.intbyte.wizbuddy.employeeworkingpart.command.infrastructure.service;
-//
-//import com.intbyte.wizbuddy.employeeworkingpart.command.application.service.EmployeeWorkingPartService;
-//import com.intbyte.wizbuddy.employeeworkingpart.command.domain.aggregate.entity.EmployeeWorkingPart;
-//import com.intbyte.wizbuddy.weeklyschedule.query.dto.WeeklyScheduleDTO;
-//
-//import com.intbyte.wizbuddy.weeklyschedule.query.service.WeeklyScheduleService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class ScheduleInfraServiceImpl implements ScheduleInfraService{
-//
-//    private final WeeklyScheduleService weeklyScheduleService;
-//    private final EmployeeWorkingPartService employeeWorkingPartService;
-//
-//
-//    @Override
-//    public WeeklyScheduleDTO findScheduleByScheduleCode(int scheduleCode){
-//        return weeklyScheduleService.findScheduleByScheduleCode(scheduleCode);
-//    }
-//
-//    @Override
-//    public EmployeeWorkingPart getEmployeeWorkingPartCode(int workingPartCode) {
-//        return employeeWorkingPartService.findEmployeeWorkingPartCode(workingPartCode);
-//    }
-//}
+package com.intbyte.wizbuddy.employeeworkingpart.command.infrastructure.service;
+
+import com.intbyte.wizbuddy.employeeworkingpart.command.application.service.EmployeeWorkingPartService;
+import com.intbyte.wizbuddy.employeeworkingpart.command.domain.aggregate.entity.EmployeeWorkingPart;
+import com.intbyte.wizbuddy.weeklyschedule.query.dto.WeeklyScheduleDTO;
+
+import com.intbyte.wizbuddy.weeklyschedule.query.service.WeeklyScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleInfraServiceImpl implements ScheduleInfraService{
+
+    private final WeeklyScheduleService weeklyScheduleService;
+    private final EmployeeWorkingPartService employeeWorkingPartService;
+
+
+    @Override
+    public WeeklyScheduleDTO findScheduleByScheduleCode(int scheduleCode){
+        return weeklyScheduleService.findScheduleByScheduleCode(scheduleCode);
+    }
+
+    @Override
+    public EmployeeWorkingPart getEmployeeWorkingPartCode(int workingPartCode) {
+        return employeeWorkingPartService.findEmployeeWorkingPartCode(workingPartCode);
+    }
+}
